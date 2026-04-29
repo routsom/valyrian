2237628c90b19e0fa879fd79aaa838513a62a7a6
Som Rout <connect@somrout.com>
2026-04-29 15:18:30 +0100

    🚀

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+    {
+        files: ['src/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+        },
+        plugins: { '@typescript-eslint': tsPlugin },
+        rules: {
+            ...tsPlugin.configs['recommended'].rules,
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+            ],
+        },
+    },
+    { ignores: ['dist/**', 'node_modules/**', 'tests/**'] },
+    prettier,
+];
